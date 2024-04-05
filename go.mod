853b3d236916fd99f3f1e49122342ae2020f6928
javiwebjr <urrutia_987@hotmail.com>
2024-04-05 15:03:37 -0600

    Go modules explanation

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
-module Constraints
+module github.com/bazzebazz/go-modules
 
 go 1.22.1
 
-require golang.org/x/exp v0.0.0-20240404231335-c0f41cb1a7a0 // indirect
+require (
+	golang.org/x/exp v0.0.0-20240404231335-c0f41cb1a7a0
+	rsc.io/quote/v3 v3.1.0
+)
+
+require (
+	golang.org/x/text v0.14.0 // indirect
+	rsc.io/sampler v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/bazzebazz/go-modules/slices"
+	"rsc.io/quote/v3"
 )
 
 func main() {
-	fmt.Println("Hello gophers")
+	list := []string{"GolaNG", "GopherS", "JaVi", quote.HelloV3()}
+	slices.Filter(list, func(item string) bool {
+		return strings.HasPrefix(strings.ToLower(item), "h")
+	})
+
+	fmt.Println(quote.Concurrency())
 }
